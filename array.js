0786cfbe951a165d14dabda61b9c8ec6adff565d
selimreza777 <selimrezacse@gmail.com>
2025-08-03 00:26:19 +0600

    array file

---
+++ array.js
@@ -0,0 +1,11 @@
+//const num1=20;
+//const num1=120;
+//const num1=220;
+//const num1=203;
+//const num1=205;
+
+//array is a multiple collection single variable name
+const numbers = [12, 11, 13, 34, 65, 45];
+const friends=[abul, kabul, motin, soarika, noyon];
+const passed= [true, false,true,false, true, false];
+const mixedArray=[12,'dhaka',true,45.66];  
